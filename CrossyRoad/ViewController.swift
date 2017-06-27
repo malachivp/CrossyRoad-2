ef8481022162c7ef3b840f4ff18fb5620c0c05b2
Malachi Poole <malachi@malachis-macbook-air.local>
2017-06-26 22:34:01 -0700

    Initial Commit

---
+++ CrossyRoad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CrossyRoad
+//
+//  Created by Malachi Poole on 6/26/17.
+//  Copyright © 2017 Malachi Poole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
