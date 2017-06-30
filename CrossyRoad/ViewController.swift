3269864067d8c9de70a9672553324573d691274a
Malachi Poole <malachi@Malachis-MacBook-Air.local>
2017-06-30 08:32:41 -0700

    first commit

--- CrossyRoad/ViewController.swift
+++ CrossyRoad/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var LandSc: [UIImageView]!
+    @IBOutlet weak var plat: UIImageView!
+    @IBOutlet weak var player: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.move))
+        player.addGestureRecognizer(tapGesture)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,47 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func move(){
+        print("hey im inside move")
+        UIView.animate(withDuration: 0.2, delay: 0, options: UIViewAnimationOptions.curveLinear, animations: {
+            
+            self.player.center = CGPoint(x: self.player.center.x, y: self.player.center.y - 50)
+            
+        }, completion: nil)
+        
+    
+        UIView.animate(withDuration: 0.8, delay: 0, options: UIViewAnimationOptions.curveLinear, animations: {
+            
+            self.player.center = CGPoint(x: self.player.center.x, y: self.player.center.y + 50)
+            
+        
+        for i in 0...self.LandSc.count-1{
+            
+                self.LandSc[i].center = CGPoint(x: self.LandSc[i].center.x, y: self.LandSc[i].center.y + 50)
 
+            }
+            
+            
+            }, completion: nil)
+        
+        
+        
+        for i in 0...self.LandSc.count-1{
+            
+            if (self.LandSc[i].center.y >= self.view.center.y * 2 + 100){
+                self.LandSc[i].center.y = -25
+            }
+       
+        }
+
+      
+
+    }
+    
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        move()
+    }
 }
 
